32d5126ea2e321c6372e11aa3b83a2127f4eec8e
kianibound <kianibound@gmail.com>
2026-08-09 12:11:02 +0330

    added CRUD logic for users in service

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,13 +1,33 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  id!: string;
+  id!: number;
 
   @Column()
   email!: string;
 
   @Column()
   password!: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log(`Inserted User with Id ${this.id}`);
+  }
+  @AfterUpdate()
+  logUpdated() {
+    console.log(`Updated User with Id ${this.id}`);
+  }
+  @AfterRemove()
+  logRemove() {
+    console.log(`Removed User with Id ${this.id}`);
+  }
 }
