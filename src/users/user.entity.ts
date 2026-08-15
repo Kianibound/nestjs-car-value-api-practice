4483e9500eea4dc4f12d75c0f7fda6c5a211d142
kianibound <kianibound@gmail.com>
2026-08-15 14:07:17 +0330

    Add report dto for expose needed data + user id

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,12 +1,13 @@
-import { Exclude } from 'class-transformer';
 import {
   AfterInsert,
   AfterRemove,
   AfterUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { Report } from '../reports/report.entity';
 
 @Entity()
 export class User {
@@ -18,6 +19,9 @@ export class User {
 
   @Column()
   password!: string;
+
+  @OneToMany(() => Report, (report) => report.user)
+  reports!: Report[];
 
   @AfterInsert()
   logInsert() {
