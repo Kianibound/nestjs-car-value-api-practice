a31250a613773bd31228e8fb8d91bf9e51af5f57
kianibound <kianibound@gmail.com>
2026-08-16 14:52:29 +0330

    Add middleware for current user

---
+++ src/guards/admin.gaurd.ts
@@ -0,0 +1,12 @@
+import { CurrentUser } from './../users/decorators/current-user.decorator';
+import { CanActivate, ExecutionContext } from '@nestjs/common';
+
+export class AdminGuard implements CanActivate {
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    if (!request.currentUser) {
+      return false;
+    }
+    return request.currentUser.admin;
+  }
+}
