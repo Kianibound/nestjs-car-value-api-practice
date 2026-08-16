688c9041d2f9f3d4b7bc823d3550353e952ba200
kianibound <kianibound@gmail.com>
2026-08-16 17:19:30 +0330

    Add estimate reports feature

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,18 +1,30 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '../users.service';
+import { User } from '../user.entity';
+
+declare global {
+  namespace Express {
+    interface Request {
+      session?: {
+        userId?: number;
+        [key: string]: any;
+      };
+      currentUser?: User | null;
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(private usersService: UsersService) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
-    const { userId } = (req as any).session || {};
+    const { userId } = req.session || {};
 
     if (userId) {
       const user = await this.usersService.findOne(userId);
 
-      //@ts-ignore
       req.currentUser = user;
     }
 
